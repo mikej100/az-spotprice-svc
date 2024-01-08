ba9528aadf39cc2f14ccf83c4a487654fef366df
mikej100 <mike.jennings@chwd.uk>
2024-01-08 17:20:17 +0000

    Get the servie working in container

--- test/py_test/test_spot_price_service.py
+++ test/py_test/test_spot_price_service.py
@@ -1,8 +1,10 @@
 from datetime import datetime, timezone
 from bson.objectid import ObjectId
+import subprocess
+import sys
 
 import src.python.az_spotprice_service as sp
-import src.python.mongo_manager as mm
+import mongo_manager as mm
 
 def test_fetch_prices():
     price_data = sp.fetch_price()
@@ -15,3 +17,6 @@ def test_write_to_mongo():
     latest_id_time = ids[-1].generation_time
     seconds_since_written = (datetime.now(timezone.utc) - latest_id_time).seconds
     assert(seconds_since_written < 1.1)
+
+def test_argparse():
+    pid = subprocess.Popen([sys.executable, "az_spotprice_service.py --freq 1 --count 3"])
