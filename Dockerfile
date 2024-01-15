891fcfed0011a79f6fc653ccf3474374fd9638d2
mikej100 <mike.jennings@chwd.uk>
2024-01-15 16:42:31 +0000

    Running on azure container instance

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,11 @@ RUN pip3 install -r requirements.txt
 
 COPY . .
 COPY ./src ./src
-COPY ./log ./log
+COPY ./log/.gitkeep ./log/.gitkeep
 COPY ./logging.yaml ./logging.yaml
 
 EXPOSE 6800
 
-CMD ["python3",  "az_spotprice_svc.py", "-i", "2", "-c", "5" ]
+CMD "echo $PATH" 
+# CMD ["python3",  "az_spotprice_svc.py", "-i 3"]
 # CMD ["kpython3 ./az_spotprice_svc.py --interval 1 --count 5"]
--- mongo_manager.py
+++ mongo_manager.py
@@ -2,13 +2,16 @@ import logging
 from pymongo import MongoClient
 import settings
 
+logger=logging.getLogger()
+
 def write_to_mongo(data):
     client = MongoClient(settings.MONGO_URI)
     db = client[settings.MONGO_DB]
-    logging.debug("Connected to MongoDB") 
 
     coll = db["spot_prices"]
+    logger.debug("Calling MongoDB insert_many")
     result = coll.insert_many(data)
+    logger.debug("Returned from MongoDB insert_many")
 
     client.close()
     return result
